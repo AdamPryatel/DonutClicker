d61084febe5aa6fa46c0145d4215ba5ac91613c3
Adam Pryatel <adampryatel@gmail.com>
2021-03-15 21:53:10 -0400

    made adjustments to Feature 1, started Feature 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
+// Dom
+ const total = document.getElementById('totalDonuts');
+ const Maker = document.getElementById('donutMaker');
+
+// Game Variables
+ let value = 0;
+
+
+
 // Feature 1 : Add to donut count and retrieve a donut count
 function add_donutCount()
 {
@@ -9,4 +18,17 @@ function add_donutCount()
     console.log(value);
     document.getElementById('totalDonuts').innerHTML = value;
 }
-add_donutCount()
+add_donutCount() 
+
+
+// Feature 2 : Be able to purchase the first Auto Clicker with 100 donuts from your donut count.
+ // Feature 2.a : Buy AutoClicker
+//  function add_Autoclicker(){
+
+//     const autoClicker = document.getElementById('addClicker');
+
+//     autoClicker.addEventListener("click", ()=> {
+//         clicks -= 100;
+//     })
+
+//  }
