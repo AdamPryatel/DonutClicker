3c4f3521f91bf72c54b15a377a703991d520b87e
Adam Pryatel <adampryatel@gmail.com>
2021-03-20 14:29:58 -0400

    added appjs

---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+ // Dom
+ const total = document.getElementById('totalDonuts');
+
+ const totalAuto = document.getElementById('totalAutoClickers') ;
+ const autoButton = document.getElementById('addClicker');
+
+ const totalMultiplier = document.getElementById('totalMultipliers');
+ const multiplierButton = document.getElementById('addMultiplier');
+
+ const valuePerClick = document.getElementById('perClick');
+
+let myDonut = new Donut();
+
+
+function updateText()                   // Interacts w/ HTML
+{
+    total.innerText = myDonut.getDonutCount();
+    totalAuto.innerText = myDonut.getautoClicker();
+    totalMultiplier.innerText = myDonut.getdonutMultiplierCount();
+    valuePerClick.innerText = myDonut.getdonutsEarnedPerClick();
+};
+
+// Feature 1 : Add to donut count and retrieve a donut count
+function add_donutCount()
+{
+    myDonut.add_donutCount();
+    updateText();
+};
+
+
+
+// feature 2           
+function auto()     // Updates values in JS by 1
+{
+
+myDonut.buy_autoClicker();
+
+  updateText();
+  
+}
+
+
+function autoUpdate(){                  // adds to the value in JS 
+
+   updateText();
+}
+
+setInterval(updateText, 1000);
+
+
+// Iteration 2 feature 1
+function Multiplier()
+{
+ myDonut.buy_donutMultiplier();
+ updateText();
+} 
+
+function multiplierUpdate(){
+ updateText();
+}
